ea8fe9026e2ea8d792ba608d47756359db80baba
Amr Angry <amr.elghadban@gmail.com>
2025-02-08 15:31:02 +0400

    Init project files

---
+++ WowPizza/WowPizzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  WowPizzaApp.swift
+//  WowPizza
+//
+//  Created by Amr Ahmed Elghadban on 08/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct WowPizzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WowPizzaTests/WowPizzaTests.swift
@@ -0,0 +1,16 @@
+//
+//  WowPizzaTests.swift
+//  WowPizzaTests
+//
+//  Created by Amr Ahmed Elghadban on 08/02/2025.
+//
+
+import Testing
+
+struct WowPizzaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
