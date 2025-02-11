3343856cd7e9a3e348d058723bf39c7f0c063af6
Amr Angry <amr.elghadban@gmail.com>
2025-02-11 21:04:01 +0400

    Create FooterView.swift

---
+++ WowPizza/CommonViews/FooterView.swift
@@ -0,0 +1,23 @@
+//
+//  FooterView.swift
+//  WowPizza
+//
+//  Created by Amr Ahmed Elghadban on 08/02/2025.
+//
+
+import SwiftUI
+
+struct FooterView: View {
+    var body: some View {
+        HStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+    }
+}
+
+#Preview {
+    FooterView()
+}
